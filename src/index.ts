98b98cdbd694c05a3e91e9d19f6f859426fa8f2f
peter <peterbuoy@gmail.com>
2022-02-11 01:20:07 -0800

    add name format enforcement on member update

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Client, Message, Intents } from "discord.js";
 import ready from "./listeners/ready";
 import dotenv from "dotenv";
 import { WelcomeHandler } from "./handler";
+import { userMention } from "@discordjs/builders";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const welcomeHandler = new WelcomeHandler();
 
 const client = new Client({
   intents: [
+    Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
@@ -24,6 +26,25 @@ client.on("messageCreate", async (message: Message) => {
   const command = args.shift()?.toLowerCase();
   if (command === "join") {
     welcomeHandler.handleJoin(client, message);
+  }
+});
+
+// Name Format Enforcement
+client.on("guildMemberUpdate", async (oldMember, newMember) => {
+  // Doesn't work for server owner
+  // Handle name enforcement here I guess? What happens if the bot goes down?
+  // Maybe have a name enforcement check when the bot turns on?
+  console.log("update detected");
+  console.log(`Old name was ${oldMember.displayName}`);
+  const regex = /<[A-Za-z(0-9)?]+>/;
+  if (
+    oldMember.displayName !== newMember.displayName &&
+    !regex.test(newMember.displayName)
+  ) {
+    const userID = oldMember.id;
+    // print 2 staff channel
+    console.log(`Invalid name change detected! for ${userMention(userID)}`);
+    console.log(`Name is ${newMember.displayName}`);
   }
 });
 
