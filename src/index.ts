1ac5bfa9f9dff418d18e36d39670ab3f6d4af147
peterbuoy <peterbuoy@gmail.com>
2022-03-03 13:16:57 -0800

    Revert "add barely functional slash command"
    This reverts commit 1237360d011f72c00c6fb8d370f2229a6929a362.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import {
-  Client,
-  Collection,
-  Guild,
-  GuildMember,
-  Intents,
-  TextChannel,
-} from "discord.js";
+import { Client, Message, Intents, TextChannel } from "discord.js";
 import fs from "fs";
 import dotenv from "dotenv";
 import config from "./config.json";
@@ -15,7 +8,7 @@ dotenv.config();
 
 console.log("Bot is starting...");
 
-const client: any = new Client({
+const client = new Client({
   intents: [
     Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILDS,
@@ -27,20 +20,6 @@ const token = process.env.BOT_TOKEN;
 
 client.login(token);
 
-client.commands = new Collection();
-
-const commandFiles = fs
-  .readdirSync("./src/commands")
-  .filter((file) => file.endsWith(".js"));
-(async () => {
-  for (const file of commandFiles) {
-    const command = await import(`./src/commands/${file}`);
-    // Set a new item in the Collection
-    // With the key as the command name and the value as the exported module
-    client.commands.set(command.data.name, command);
-  }
-})();
-
 const eventFiles = fs
   .readdirSync("./src/events")
   .filter((file) => file.endsWith(".ts"));
@@ -50,15 +29,15 @@ const eventFiles = fs
   for (const file of eventFiles) {
     const event = await import(`./events/${file}`);
     if (event.once) {
-      client.once(event.name, (...args: any) => event.execute(...args));
+      client.once(event.name, (...args) => event.execute(...args));
     } else {
-      client.on(event.name, (...args: any) => event.execute(...args));
+      client.on(event.name, (...args) => event.execute(...args));
     }
   }
 })();
 
 // If you put this in events it won't detect the removal
-client.on("guildMemberRemove", (member: GuildMember) => {
+client.on("guildMemberRemove", (member) => {
   console.log("guild member removed");
   const staffBotNotifChannel = member.guild.channels.cache.get(
     config.chan_staff_bot_notif
@@ -71,24 +50,6 @@ client.on("guildMemberRemove", (member: GuildMember) => {
         member.displayName
       )} has left the server. Please kick them in game`
     );
-  }
-});
-
-client.on("interactionCreate", async (interaction: any) => {
-  if (!interaction.isCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
   }
 });
 
