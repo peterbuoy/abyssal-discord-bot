aa53f1baf16c4be1c0b04e6823202e7ea7dc52b0
peter <peterbuoy@gmail.com>
2022-03-10 21:44:16 -0800

    add member to dump sheet if they get detagged

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -1,4 +1,9 @@
-import { GuildMember, PartialGuildMember, TextChannel } from "discord.js";
+import {
+  Collection,
+  GuildMember,
+  PartialGuildMember,
+  TextChannel,
+} from "discord.js";
 import { userMention } from "@discordjs/builders";
 import config from "../config.json";
 import { addToSheet } from "../utils/addToSheet";
@@ -7,10 +12,14 @@ import { updateSheetFamilyName } from "../utils/updateSheetFamilyName";
 import { sendWelcomeMessage } from "../utils/sendWelcomeMessage";
 
 import utils from "../utils/utils";
+import { getSheetByTitle } from "src/utils/getSheetByTitle";
 
 module.exports = {
   name: "guildMemberUpdate",
-  execute(oldMember: GuildMember | PartialGuildMember, newMember: GuildMember) {
+  async execute(
+    oldMember: GuildMember | PartialGuildMember,
+    newMember: GuildMember
+  ) {
     const userID = oldMember.id;
     const staffBotNotifChannel = newMember.guild.channels.cache.get(
       config.chan_staff_bot_notif
@@ -34,7 +43,43 @@ module.exports = {
       (oldMember.roles.cache.has(config.role_az) &&
         !newMember.roles.cache.has(config.role_az))
     ) {
-      // TODO: Add to dump sheet
+      // START: Add to dump sheet
+      let sheetTitle = "";
+      let dumpSheetTitle = "";
+      if (oldMember?.roles.cache.has(config.role_ab)) {
+        sheetTitle = config.ab_sheet_title;
+        dumpSheetTitle = config.ab_dump_sheet_title;
+      } else if (oldMember?.roles.cache.has(config.role_az)) {
+        sheetTitle = config.az_sheet_title;
+        dumpSheetTitle = config.az_dump_sheet_title;
+      } else {
+        throw Error("Member does not have a valid role");
+      }
+      // This functionality to add member info to the dumpsheet is pulled from
+      // the update command. Consider writing a function?
+      const sheet = await getSheetByTitle(sheetTitle);
+      const rows = await sheet?.getRows();
+      const targetRow = rows?.find(
+        (row) => row["Discord UserID"] === oldMember.user.id
+      );
+      const dumpSheet = await getSheetByTitle(config.ab_dump_sheet_title);
+      // Move current info to Dump Sheet
+      if (targetRow !== undefined) {
+        await dumpSheet?.addRow({
+          "Discord UserID": targetRow["Discord UserID"],
+          "Family Name": targetRow["Family Name"],
+          "Character Name": targetRow["Character Name"],
+          Class: targetRow["Class"],
+          Level: targetRow["Level"],
+          "Gear Score": targetRow["Gear Score"],
+          AP: targetRow["AP"],
+          "Awaken AP": targetRow["Awaken AP"],
+          DP: targetRow["DP"],
+          "Gear Screenshot": targetRow["Gear Screenshot"],
+          "Join Date": targetRow["Join Date"],
+        });
+      }
+      // END add to dump sheet
       removeFromSheet(oldMember);
       // TODO: remove from war signup if member is in abyssal
     }
