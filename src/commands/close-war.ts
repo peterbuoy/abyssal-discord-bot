994c17d1f3daa3b846961a38b3c421f43b72b907
peter <peterbuoy@gmail.com>
2022-03-15 13:41:29 -0700

    refactor close-war for readability

--- src/commands/close-war.ts
+++ src/commands/close-war.ts
@@ -7,7 +7,7 @@ import { TextChannel } from "discord.js";
 export default {
   name: "close-war",
   category: "Testing",
-  description: "closes war signup sheet",
+  description: "closes currently active war",
   slash: false,
   testOnly: true,
   minArgs: 0,
@@ -15,7 +15,6 @@ export default {
   syntax: "close-war",
   cooldown: "5s",
   callback: async ({ client, member, message, channel }) => {
-    // Nice to have: await user confirmation that this command will overwrite the active war
     // Only allow in #warbot-spam and only usable by war staff
     if (
       channel.id !== config.chan_war_bot_spam ||
@@ -26,29 +25,26 @@ export default {
     const currentWar = await pool.query(
       "UPDATE warsignup SET is_active = false WHERE is_active = true RETURNING name, date_of_war"
     );
-    if (currentWar.rowCount === 1) {
-      message.reply(
-        `**${currentWar.rows[0].name}** was closed by <${utils.parseFamilyName(
-          member.displayName
-        )}>`
-      );
-      const attendanceChannel = client.channels.cache.get(
-        config.chan_attendance_log
-      ) as TextChannel;
-      const nodeWarSignupChan = client.channels.cache.get(
-        config.chan_node_war_signup
-      ) as TextChannel;
-      attendanceChannel.send(
-        `**${currentWar.rows[0].name}** was closed by <${utils.parseFamilyName(
-          member.displayName
-        )}>`
-      );
-      nodeWarSignupChan.messages
-        .fetch()
-        .then((messages) => messages.forEach((m) => m.delete()));
-    } else {
-      console.error("Error: more than one war was closed.");
-      message.reply("There is no war to close!");
+    if (currentWar.rowCount !== 1) {
+      return message.reply("There is no war to close!");
     }
+
+    message.reply(
+      `**${currentWar.rows[0].name}** was closed by <${utils.parseFamilyName(
+        member.displayName
+      )}>`
+    );
+    const attendanceChannel = client.channels.cache.get(
+      config.chan_attendance_log
+    ) as TextChannel;
+    const nodeWarSignupChan = client.channels.cache.get(
+      config.chan_node_war_signup
+    ) as TextChannel;
+    attendanceChannel.send(
+      `**${currentWar.rows[0].name}** was closed by <${utils.parseFamilyName(
+        member.displayName
+      )}>`
+    );
+    nodeWarSignupChan.bulkDelete(10);
   },
 } as ICommand;
