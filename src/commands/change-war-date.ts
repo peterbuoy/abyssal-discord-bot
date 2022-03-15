c9d508fffe176672e76e01633490c0c054a3e2fc
peter <peterbuoy@gmail.com>
2022-03-15 03:42:12 -0700

    add cmd to change war date

---
+++ src/commands/change-war-date.ts
@@ -0,0 +1,55 @@
+import pool from "../db/index";
+import { ICommand } from "wokcommands";
+import config from "../config.json";
+import utils from "../utils/utils";
+import { Collection, MessageEmbed, TextChannel } from "discord.js";
+import dayjs from "dayjs";
+
+export default {
+  name: "change-war-date",
+  category: "Testing",
+  description: "closes war signup sheet",
+  slash: false,
+  testOnly: true,
+  minArgs: 1,
+  maxArgs: 1,
+  syntax: "change-war-date",
+  cooldown: "5s",
+  callback: async ({ client, member, message, args }) => {
+    const date = dayjs(args[0]).format("MM/DD/YYYY");
+    const currentWar = await pool.query(
+      "UPDATE warsignup SET date_of_war = $1 WHERE is_active = true RETURNING *",
+      [date]
+    );
+    message.reply(`You have changed the date of the current war to ${date}`);
+    const attendanceChannel = client.channels.cache.get(
+      config.chan_attendance_log
+    ) as TextChannel;
+    attendanceChannel.send(
+      `🎉 The event ${
+        currentWar.rows[0].name
+      } has had its date changed to ${date} by <${utils.parseFamilyName(
+        member.displayName
+      )}>🎉 `
+    );
+    const nodeWarSignupChan = client.channels.cache.get(
+      config.chan_node_war_signup
+    ) as TextChannel;
+    nodeWarSignupChan.messages.fetch().then((messages) => {
+      const embedMessage = messages.find(
+        (message) => message.author.bot && message.embeds.length > 0
+      );
+      const newEmbed = new MessageEmbed(embedMessage?.embeds[0]).setFields(
+        {
+          name: "Date",
+          value: date,
+        },
+        {
+          name: "Instructions",
+          value: "Click the 🅱️ below to sign up or unsignup from war",
+        }
+      );
+      embedMessage?.edit({ embeds: [newEmbed] });
+    });
+  },
+} as ICommand;
