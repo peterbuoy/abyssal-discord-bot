a454a113277b387ead5cb29790337ce3afb72a9f
peter <peterbuoy@gmail.com>
2022-03-23 13:30:18 -0700

    add confirmation msg for vacation and error handling

--- src/commands/vacation.ts
+++ src/commands/vacation.ts
@@ -12,7 +12,7 @@ export default {
   callback: async ({ member, channel, args, message }) => {
     // only allow cmd to be used by ab az guild memebers in contact an officer chan
     if (
-      member.roles.cache.hasAny(config.role_ab, config.role_az) ||
+      !member.roles.cache.hasAny(config.role_ab, config.role_az) ||
       channel.id !== config.chan_contact_an_officer
     ) {
       message.reply("You can only use this in the contact an officer channel.");
@@ -21,13 +21,22 @@ export default {
     let sheetTitle = "";
     // if they have both they'll be put into ab sheet
     // this should never happen unless war staff, admin, or az gm mess up
-    member.roles.cache.has(config.role_ab)
-      ? (sheetTitle = config.ab_sheet_title)
-      : (sheetTitle = config.az_sheet_title);
-    const sheet = await getSheetByTitle(sheetTitle);
-    const rows = await sheet?.getRows();
-    const memberRow = rows?.find((row) => row["Discord UserID"] === member.id);
-    memberRow!["Vacation Info"] = args.join(" ");
-    memberRow?.save();
+    try {
+      member.roles.cache.has(config.role_ab)
+        ? (sheetTitle = config.ab_sheet_title)
+        : (sheetTitle = config.az_sheet_title);
+      const sheet = await getSheetByTitle(sheetTitle);
+      const rows = await sheet?.getRows();
+      const memberRow = rows?.find(
+        (row) => row["Discord UserID"] === member.id
+      );
+      memberRow!["Vacation Info"] = args.join(" ");
+      memberRow?.save();
+      message.reply("Your request has been recorded.");
+    } catch (error) {
+      throw Error(
+        `An error has occurred while trying to use the vacation command: ${error}`
+      );
+    }
   },
 } as ICommand;
