c9bb81600a920912756edd69bcaded0b2564ba5e
peter <peterbuoy@gmail.com>
2022-05-17 22:29:43 -0700

    fix removeFromSheet to only use cell ops

--- src/utils/removeFromSheet.ts
+++ src/utils/removeFromSheet.ts
@@ -11,11 +11,29 @@ const removeFromSheet = async (oldMember: GuildMember | PartialGuildMember) => {
   }
   try {
     const sheet = await getSheetByTitle(sheetTitle);
-    const rows = await sheet?.getRows();
-    // use forEach to remove potential repeated members (happened before)
-    rows?.forEach(async (row) => {
-      if (row["Discord UserID"] === oldMember.id) await row.delete();
-    });
+    await sheet?.loadCells("A2:W");
+    const rowCount = sheet?.rowCount ?? 100;
+    for (let rowIndex = 1; rowIndex < rowCount; rowIndex++) {
+      // Check cell if ID matches leaving member
+      if (sheet?.getCell(rowIndex, 0).value === oldMember.id) {
+        console.log(oldMember.id);
+        // delete col 1 and 2
+        // col 2 can NOT be an empty string or the formulas will break
+        sheet.getCell(rowIndex, 0).value = "";
+        sheet.getCell(rowIndex, 1).value = false;
+        // delete index 9 - 19, aka column 10 to 20
+        for (let col = 9; col < 21; col++) {
+          sheet.getCell(rowIndex, col).value = "";
+        }
+        await sheet.saveUpdatedCells();
+        break;
+      }
+    }
+    // const rows = await sheet?.getRows();
+    // // use forEach to remove potential repeated members (happened before)
+    // rows?.forEach(async (row) => {
+    //   if (row["Discord UserID"] === oldMember.id) await row.delete();
+    // });
   } catch (error) {
     console.error(error);
   }
