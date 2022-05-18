f5d9cc225255efec160bbf664875052b592ea651
peter <peterbuoy@gmail.com>
2022-05-17 23:59:42 -0700

    fix list count being off
    bug caused by switching from row to cell ops in google sheet

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -48,7 +48,7 @@ export default {
     const memberCollection: Collection<string, [string, string]> =
       new Collection();
     rows?.forEach((row) => {
-      if (row["Discord UserID"] !== undefined) {
+      if (row["Discord UserID"] !== "") {
         memberCollection.set(row["Discord UserID"], [
           row["Family Name"],
           row["Vacation Info"],
