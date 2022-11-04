c13db2146b5f1b2dd5d538812b725ccee68ba4fa
peterbuoy <peterbuoy@gmail.com>
2022-11-03 17:53:21 -0700

    Add support for embing medal.tv clips

---
+++ src/commands/medal.ts
@@ -0,0 +1,46 @@
+import { ICommand } from "wokcommands";
+import * as cheerio from "cheerio";
+
+export default {
+  name: "medal",
+  category: "Fun",
+  description:
+    "Put your Medal clip sharing link as a command argument to play the video in Discord.",
+  slash: false,
+  testOnly: false,
+  minArgs: 1,
+  maxArgs: 1,
+  expectedArgs: "<Medal clip sharing link here>",
+  syntax:
+    "medal <https://medal.tv/games/black-desert/clips/H2GC0kmyMJt0a/ZJoPPMFO5SH4?theater=true>",
+  cooldown: "2s",
+  callback: async ({ channel, args }) => {
+    let link = args[0];
+    // Validate hostname
+    const linkHostName = new URL(link).hostname;
+    if (linkHostName !== "medal.tv") {
+      console.log("Sorry, that's not a valid medal.tv link");
+      return;
+    }
+    // Sometimes sharing links use the clips route which has different html structure from clip route
+    link = link.replace("clips", "clip");
+    try {
+      const res = await fetch(link);
+      const html = await res.text();
+      const $ = cheerio.load(html);
+      const directLink: string | undefined = $('[property="og:video"]').attr(
+        "content"
+      );
+      if (directLink == undefined) {
+        console.error("Unable to obtain direct link to video using cheerio.");
+        return;
+      }
+      channel.send(directLink);
+    } catch (error) {
+      console.log(
+        "Unexpected error while obtaining video from given medal link."
+      );
+      console.error(error);
+    }
+  },
+} as ICommand;
