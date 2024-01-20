149ef6129bcfecc1c15731b739052c7c9bce1a2d
peterbuoy <peterbuoy@gmail.com>
2024-01-20 11:28:16 -0800

    update postgres to use supavisor connection string

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,10 +2,9 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
+const connectionString = process.env.SUPABASE_CONN_STR;
 const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString,
 });
+
 export { pool };
